4e5d392b23b57034747f0f1cfd8d9ad51a327216
orayb-alsmadi <oraybalsmadi@yahoo.com>
2019-07-15 13:25:25 +0300

    quastion 9

--- array.js
+++ array.js
@@ -133,7 +133,9 @@ nums => [5,-22,3.5,44,98,44]
 */
 
 var animals = ['cat', 'ele', 'bird']
-animals= ['zebra', 'elephant'];
+animals.length = 2;
+animals[0]= 'zebra'; 
+animals[1]= 'elephant';
 
 var nums= [1,2,3,8,9];
 nums[0]= 5;
